e067a0c2451b7f29efb82b5c480694ac1f310e58
Nichole Huffman <nicholehuffman@Nicholes-MacBook-Air.local>
2020-11-28 12:11:41 -0600

    Add sample config and some data

---
+++ prompt_data.js
@@ -0,0 +1,28 @@
+const dialogue_prompts = [
+    'Two characters are strategizing a plan.',
+    'Two characters are arguing.',
+    'A character has professed their feelings for another character.',
+    'Two characters are discussing another character who is not present.',
+    'A character is trying to get information from another.',
+    'A character is trying to get your character to express how they feel about something.',
+]
+
+const character_prompts = [
+    'Your character is robbed.',
+    'Your character is injured.',
+    'Your character is at a party.',
+    'Your character is being held captive.',
+    'Your character witnesses an injustice.',
+    'Two characters want the same item but there is only one.',
+    'Someone tells your character no.',
+    'Someone insults your character.',
+    'Someone questions your character’s motives',
+]
+
+const rework_these = [
+    'Describe the moment when your character knows when they are in love. | I know I\'m in love when _____________.',
+    'What is your character lying to themselves about? | Are you lying to yourself about anything?',
+    'Is there anyone who\'s changed your life but doesn\'t know it?',
+    'What do you need right now, more than anything?',
+    
+]
