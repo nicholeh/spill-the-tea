2845604d2863fe9ad9e9f79bd59a0fecbc0f7668
Nichole Huffman <nicholehuffman@Nicholes-MacBook-Air.local>
2020-12-05 14:19:21 -0600

    Finish organizing and fleshing out remaining prompts.

--- data/prompt_data.js
+++ data/prompt_data.js
@@ -11,27 +11,77 @@ const dialogue_prompts = [
     '*\"What do you need right now, more than anything?\"* A character is upset and your character doesn\'t know what to do.',
     '*\"I\'m sorry, but I don\'t feel the same way about you.\"*',
     '*\"Don\’t trust them.\"*',
+    '*\"What compliment do you think I hear the most?\"* How does a character perceive the other?',
+    '*\"What about me intrigued you when we met?\"*',
+    '*\"What was your first impression of me?\"*',
+    '*\"Do I remind you of anyone?\"*',
+    '*\"What is the first thing you noticed about me?\"*',
+    '*\"What do you think is the hardest part of what I do for a living?\"* How does one character perceive the other\'s workload?',
+    'What lesson took the longest for your character to unlearn? Write it as a dialogue between characters.',
+    'Your character tells a lie to someone important to them.',
+    '*\"What are you still trying to prove to yourself?\"* Two characters get into an argument over another\'s behaviour',
+    '*\"You are lying to yourself.\"* Two characters get into an argument.',
+    '*\"What is your first love\'s name and the reason you fell in love with him/her?\"* Your character and their love interest talk about the past.',
+    'What part of your character\'s life is working? What part of their life hurts? Write a dialogue between two characters where they are vulnerable about what is good or hard about their life.',
+    '*\"Do you believe everyone has a calling?\"* A character struggles to find their purpose and confides in another.',
+    '*\"What do you admire most about me?\"*',
+    '*\"What about me is hardest for you to understand?\"*',
+    'What is your character most qualified to give advice about? Write a dialogue where they try to prevent another character from making the same mistakes.',
 ]
 
 const character_prompts = [
+    'Pick out a previous scene you have written of a conversation between two characters. What does one of the characters remember most about the conversation? Write their inner dialogue as they recall it.',
     'Two characters want the same item but there is only one.',
     'Someone tells your character no.',
-    'Someone insults your character.',
-    'Someone questions your character’s motives.',
+    'Someone insults your character. How do they react?',
+    'Someone questions your character’s motives. How do they react?',
     'Write a character\'s inner monologue during the moment they realize they are in love.',
     'What is your character lying to themselves about? Another character confronts them about it.',
     'Is there anyone who changed your character\'s life that doesn\'t know it? Write your character either telling that person or describing how much that character means to them to someone else.',
-    'Your character has become increasingly obsessed with something or someone. Write their inner monologue as the object of their obsession begins to consume them.'
+    'Your character has become increasingly obsessed with something or someone. Write their inner monologue as the object of their obsession begins to consume them.',
+    '*\"Do you think I intimidate others? Why or why not?\"* What does one character perceive about the other?',
+    'Your characters have evolved during their journey. What feels strange or unfamiliar now to your character?',
+    'Does your character appear to be kind? Harsh? Or like a pushover? Describe your character from someone else\'s point of view.',
+    '*\"Do you think I fall in love easily?\"* What does one character perceive about the other?',
+    'What cause does your character passionately advocate for or against?',
+    'Your character finds themselves suspicious of someone they thought they trusted and have to decide whether to trust them or find evidence to validate their suspicion.',
+    'How would your character describe the feeling of being in love to another?',
+    'Someone asks your character how they are doing. What would it look like if they answered truthfully?',
+    'What does your character crave more of? Love? Attention? What else?',
+    'What dream did your character let go of?',
+    'Write a moment where your character is missing someone.',
+    'What is your characters earliest recollection of happiness?',
+    'How does one earn your character\'s vulnerability? Has their love interest earned it? How can they earn it?',
+    'Your character tells someone \"I love you\" but doesn\'t mean it? Why?',
+    'What was the moment your character realized they weren\'t invincible?',
+    'What is the most pain your character has ever been in that wasn\'t physical? Heartbreak, grief, etc. Write the moment they felt it for the first time.',
+    'What question is your character trying to answer most in their life right now? Write their inner monologue.',
+    '*\"Why do you think we met?\"*',
+    '*\"What do you think my weakness is?\"* Write one character\'s perception of another.',
+    '*\"What do you think I fear the most?\"* Write one character\'s perception of another.',
+    '*\"What do you recommend I let go of?\"* Write one character\'s perception of another.',
+    'Character A admits something to another character.',
+    '*\"What parts of yourself do you see in me?\"* Write one character\'s perception of another.',
+    'What does one character know about another that perhaps that character is unaware of?',
+    'How would once character describe the other to a stranger?',
+    'What does one character need from another that would make them feel closer to them?',
+    'Write the moment when one character learns something that surprises them about another character.',
 ]
 
 const situation_prompts = [
     'A character is robbed.',
     'A character is injured.',
-    'A character is at a party.',
-    'A character is being held captive.',
+    'A character is at a party. Where do you find them? What are they doing?',
+    'A character is being held captive. How do they react?',
     'A character witnesses an injustice that goes against their moral beliefs. What do they do?',
     'One character decides to follow/stalk another.',
     'One of your characters goes missing.',
+    'Two characters exchange presents. What do they get each other?',
+    'Character B makes an assumption about Character A.',
+    'Character A pushes Character B to do something outside of their comfort zone',
+    'Write the moment when one character feels the most connected to another.',
+    'Write a character receiving their perfect gift.',
+
 ]
 
 const spicy_prompts = [
@@ -40,69 +90,10 @@ const spicy_prompts = [
 
 const abstract_prompts = [
     'Write a scene that captures the sadness of the word "almost"',
+    'Write a scene with a focus on what a character\'s body language is telling the other.',
+    'Write a scene where two characters only communicate through body language.'
 ]
 
-const perception = [
-    'Do you think I intimidate others? Why or why not?',
-    'What about me is most strange or unfamiliar to you?',
-    'What compliment do you think I hear the most?',
-    'What about me intrigues you?',
-    'If you were to buy me a present, knowing nothing about me other than what I look like, what would it be?',
-    'Do I look kind? Explain.',
-    'Character B makes an assumption about Character A.',
-    'Do you think I fall in love easily? Why or why not?',
-    'What was your first impression of me?',
-    'What is my body language telling you right now?',
-    'Do I remind you of anyone?',
-    'Do you think I\'ve ever checked an ex\'s phone for evidence?',
-    'What is the first thing you noticed about me?',
-    'What do you think is the hardest part of what I do for a living?',
-]
+allPrompts.push(...dialogue_prompts, ...character_prompts, ...situation_prompts, ...spicy_prompts, ...abstract_prompts)
 
-const connection = [
-    'Write your Character\'s happiest memory.',
-    'How would you describe the feeling of being in love for your character?',
-    'Write the last lie your character told to someone important to them',
-    'Someone asks your character how they are doing. What would it look like if they answered truthfully?',
-    'What does your character crave more of? Love? Attention? What else?',
-    'What dream did your character let go of?',
-    'Write a moment where your character is missing someone.',
-    'What lesson took the longest for your character to unlearn? Write it as a dialogue between characters.',
-    'What is your characters earliest recollection of happiness?',
-    'How does one earn your character\'s vulnerability? Have I earned it? How can I earn more?',
-    'Have you ever told someone I Love you but didn\'t mean it? Why?',
-    'What are you still trying to prove to yourself?',
-    'What was the moment your character realized they weren\'t invincible?',
-    'Are you lying to yourself about anything?',
-    'What is the most pain you have ever been in that wasn\'t physical?',
-    'What part of your life works? What part of your life hurts?',
-    'What is your first love\'s name and the reason you fell in love with him/her?',
-    'What question are you trying to answer most in your life right now?',
-]
-
-const reflection = [
-    'When this conversation is over, what will you remember about me?',
-    'Why do you think we met?',
-    'What do you think my weakness is?',
-    'What do you think I fear the most?',
-    'What do you think I should know about myself that perhaps I\'m unaware of?',
-    'Do you believe everyone has a calling? Have I found mine?',
-    'What do you recommend I let go of?',
-    'What do you think our most important similarity is?',
-    'Character A pushes Character B to do something outside of their comfort zone',
-    'Character A admits something to another character.',
-    'What parts of yourself do you see in me?',
-    'How would you describe me to a stranger?',
-    'When in this game did you feel most connected to me?',
-    'What would make you feel closer to me?',
-    'What about me surprised you?',
-    'Write a character receiving their perfect gift.',
-    'What do you admire most about me?',
-    'What about me is hardest for you to understand?',
-    'What am I most qualified to give advice about?',
-    
-]
-
-allPrompts.push(...dialogue_prompts, ...character_prompts, ...situation_prompts, ...spicy_prompts, ...abstract_prompts, ...perception, ...connection, ...reflection)
-
-module.exports = { allPrompts, dialogue_prompts, character_prompts, perception, connection, reflection }
+module.exports = { allPrompts, dialogue_prompts, character_prompts, situation_prompts, spicy_prompts, abstract_prompts }
