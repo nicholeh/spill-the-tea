566ecd4cf486a76aac8afd422db21dfeb95ac58d
Nichole Huffman <nicholehuffman@Nicholes-MacBook-Air.local>
2020-12-05 16:54:34 -0600

    Add args for different categories

--- commands/getPrompt.js
+++ commands/getPrompt.js
@@ -1,5 +1,5 @@
 const tools = require('../helpers/index')
-const { allPrompts } = require('../data/prompt_data.js')
+const { allPrompts, allPromptsWithSpice, dialogue_prompts, character_prompts, situation_prompts, spice_prompts, abstract_prompts } = require('../data/prompt_data.js')
 
 class Prompts {
     getRandomPrompt(list) {
@@ -8,6 +8,30 @@ class Prompts {
     }
     getRandomPromptFromAll(message) {
         const selectedPrompt = this.getRandomPrompt(allPrompts)
+        message.channel.send(selectedPrompt);
+    }
+    getRandomPromptFromAllWithSpice(message) {
+        const selectedPrompt = this.getRandomPrompt(allPromptsWithSpice)
+        message.channel.send(selectedPrompt);
+    }
+    getRandomPromptFromDialogue(message) {
+        const selectedPrompt = this.getRandomPrompt(dialogue_prompts)
+        message.channel.send(selectedPrompt);
+    }
+    getRandomPromptFromCharacter(message) {
+        const selectedPrompt = this.getRandomPrompt(character_prompts)
+        message.channel.send(selectedPrompt);
+    }
+    getRandomPromptFromSituation(message) {
+        const selectedPrompt = this.getRandomPrompt(situation_prompts)
+        message.channel.send(selectedPrompt);
+    }
+    getRandomPromptFromSpice(message) {
+        const selectedPrompt = this.getRandomPrompt(spice_prompts)
+        message.channel.send(selectedPrompt);
+    }
+    getRandomPromptFromAbstract(message) {
+        const selectedPrompt = this.getRandomPrompt(abstract_prompts)
         message.channel.send(selectedPrompt);
     }
 }
