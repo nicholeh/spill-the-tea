8137e899df6e073c7bbcf3f3a41b819bb9267a62
Nichole Huffman <nicholehuffman@Nicholes-MacBook-Air.local>
2020-12-05 16:08:30 -0600

    add command list with som documentation on the different args.

---
+++ commands/index.js
@@ -0,0 +1,11 @@
+const commandList = {
+    'prompt': {
+        'helperText': 'Returns a single prompt. *Please note, NSFW prompts are marked with a :hot_pepper:.*',
+        'args': {
+            'spice': 'Returns a single prompt from the spice (aka NSFW) category.',
+            'dialogue': 'Returns a single prompt focused on dialog between characters.',
+            'character': 'Returns a single prompt meant to help you define your character more.',
+            'situation': 'Returns a single prompt about a situation your character finds themselves in.'
+        }
+    },
+}
