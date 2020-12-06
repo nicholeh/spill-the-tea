4e00785a25de2b0d8ab7d09d45ccb21d5151d4b0
Nichole Huffman <nicholehuffman@Nicholes-MacBook-Air.local>
2020-12-06 11:53:29 -0600

    Added in some error handling. Will probably need to refactor this soon.

--- index.js
+++ index.js
@@ -26,22 +26,34 @@ client.on('message', (message) => {
     }
 
     if (request.command === 'prompt') {
-        if (request.args && request.args.includes('dialogue')) {
-            prompt.getRandomPromptFromDialogue(message)
+        const category = request.args[0]
+        if(request.args && request.args.length > 1) {
+            message.channel.send('You have passed in too many arguments. `/prompt` only takes one argument. To see the available options, use the `/help` command.')
         }
-        else if (request.args && request.args.includes('character')) {
-            prompt.getRandomPromptFromCharacter(message)
-        }
-        else if (request.args && request.args.includes('situation')) {
-            prompt.getRandomPromptFromSituation(message)
-        }
-        else if (request.args && request.args.includes('spice')) {
-            prompt.getRandomPromptFromSpice(message)
-        }
-        else if (request.args && request.args.includes('abstract')) {
-            prompt.getRandomPromptFromAbstract(message)
-        } else {
+        else if(request.args && request.args.length === 0) {
             prompt.getRandomPromptFromAllWithSpice(message)
         }
+        else if(request.args) {
+            if (category === 'dialogue') {
+                prompt.getRandomPromptFromDialogue(message)
+            }
+            else if (category === 'character') {
+                prompt.getRandomPromptFromCharacter(message)
+            }
+            else if (category === 'situation') {
+                prompt.getRandomPromptFromSituation(message)
+            }
+            else if (category === 'spice') {
+                prompt.getRandomPromptFromSpice(message)
+            }
+            else if (category === 'abstract') {
+                prompt.getRandomPromptFromAbstract(message)
+            } else {
+                message.channel.send('I\'m sorry. I don\'t recognize that category. To see the available options, use the `/help` command.')
+            }
+        } else {
+            message.channel.send('Congratulations! :partying_face I did not anticipate this scenario and therefore I don\'t have a good error message.')
+        }
+        
     }
 })
