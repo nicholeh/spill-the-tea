711a7b18fd8528244b18d661cb81db30b4eb465e
Nichole Huffman <nicholehuffman@Nicholes-MacBook-Air.local>
2020-12-06 15:38:35 -0600

    Setting up to deploy

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const Discord = require('discord.js')
 const config = require('./config.json')
 
 const client = new Discord.Client()
-client.login(config.BOT_TOKEN)
+// client.login(config.BOT_TOKEN)
+client.login(process.env.BOT_TOKEN)
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
