368cde287b0f2fe980a9abe16be2a04d655c534d
Nichole Huffman <nicholeh@users.noreply.github.com>
2020-12-15 19:00:09 -0600

    Update help command.

--- commands/index.js
+++ commands/index.js
@@ -3,11 +3,12 @@ const commandList = {
         'command': 'prompt',
         'helperText': 'Returns a single prompt. *Please note, NSFW prompts are marked with a :hot_pepper:.*',
         'args': {
-            'dialogue': 'Returns a single prompt focused on dialog between characters.',
-            'character': 'Returns a single prompt meant to help you define your character more.',
-            'situation': 'Returns a single prompt about a situation your character finds themselves in.',
-            'spice': 'Returns a single prompt from the spice (aka NSFW) category.',
             'abstract': 'Returns an abstract prompt.',
+            'character': 'Returns a single prompt meant to help you define your character more.',
+            'dialogue': 'Returns a single prompt focused on dialog between characters.',
+            'holiday': 'Returns a :smiling_imp: or :angel: holiday themed prompt.',
+            'situation': 'Returns a single prompt about a situation your character finds themselves in.',
+            'spice': 'Returns a single prompt from the :hot_pepper: spice (aka NSFW) category.',
         }
     },
 }
